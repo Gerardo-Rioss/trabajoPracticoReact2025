ded1a351752bbcbc8cd4c6f619de9038f9a305b5
Gerardo-Rioss <gerarios26@gmail.com>
2025-05-27 11:19:37 -0300

    Trabajando en la navBar ...falta corregir la alineacion del search en el centro

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,18 +1,43 @@
 import styles from "./NavBar.module.css";
+import { FiSearch } from "react-icons/fi";
 
 export const NavBar = () => {
   return (
-    <nav className={styles.navbar}>
-      <img className={styles.logoImagen} src={"LogoMercadoLibre.jpg"} alt="" />
-      <div className={styles.search}>
-        <input type="text" placeholder="Buscar productos, marcas y más..." />
-        <button className={styles.buttonSearch} type="submit">
-          Buscar
-        </button>
-      </div>
-      <div className={styles.links}>
-        <a href="/">Inicio</a>
-        <a href="/">Categorías</a>
+    <nav>
+      <div className={styles.navContainer}>
+        <div className={styles.navTop}>
+          <div className={styles.imgContainer}>
+            <img
+              className={styles.logoImagen}
+              src={"LogoMercadoLibre.jpg"}
+              alt=""
+            />
+          </div>
+          <div className={styles.searchContainer}>
+            <div className={styles.search}>
+              <input
+                className={styles.inputSearch}
+                type="text"
+                placeholder="Buscar productos, marcas y más..."
+              />
+              <div className={styles.searchDivider}></div>
+              <button className={styles.buttonSearch} type="submit">
+                <FiSearch className={styles.searchIcon} />
+              </button>
+            </div>
+
+          </div>
+        </div>
+        <div className={styles.navBotton}>
+          <div className={styles.menuList}>
+            <div className={styles.links}>
+              <a href="/">Inicio</a>
+              <a href="/">Creá tu cuenta</a>
+              <a href="/">Ingresá</a>
+              <a href="/">Mis compras</a>
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
   );
