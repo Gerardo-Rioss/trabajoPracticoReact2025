394748bac2a0a5a63ebe773cc558f668dfdcd45e
Gerardo-Rioss <gerarios26@gmail.com>
2025-05-29 09:20:55 -0300

    Agregado buttonContainer

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,4 +1,4 @@
-import style from "./ProductCard.module.css";
+import styles from "./ProductCard.module.css";
 type ProductCardProps = {
   id: number;
   name: string;
@@ -10,19 +10,22 @@ type ProductCardProps = {
 function ProductCard(props: ProductCardProps) {
   const { id, name, price, description, image } = props;
   return (
-    <div className={style.card}>
-      <div className={style.imageContainer}>
-        <img src={image} alt={name} className={style.image} />
+    <div className={styles.card}>
+      <div className={styles.imageContainer}>
+        <img src={image} alt={name} className={styles.image} />
       </div>
-      <div className={style.cardContent}>
-        <div className={style.cardHeader}>
-          <span className={style.id}>id:{id}</span>
-          <p className={style.name}>{name}</p>
-          <p className={style.price}>${price.toFixed(2)}</p>
-          <p className={style.description}>{description}</p>
+      <div className={styles.cardContent}>
+        <div className={styles.cardHeader}>
+          <span className={styles.id}>id:{id}</span>
+          <p className={styles.name}>{name}</p>
+          <p className={styles.price}>${price.toFixed(2)}</p>
+          <p className={styles.description}>{description}</p>
         </div>
-        <button className={style.buttonCompra}>Comprar ahora</button>
-        <button className={style.buttonCarrito}>Agregar carrito</button>
+        <div className={styles.buttonContainer}>
+        <button className={styles.buttonCompra}>Comprar ahora</button>
+        <button className={styles.buttonCarrito}>Agregar carrito</button>
+
+        </div>
       </div>
     </div>
   );
