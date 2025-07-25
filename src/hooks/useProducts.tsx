338c371199ed0bbe7756545ebbe2d40eb069c48a
Gerardo-Rioss <gerarios26@gmail.com>
2025-07-25 10:28:49 -0300

    actualizado el nombre productService

--- src/hooks/useProducts.tsx
+++ src/hooks/useProducts.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getProducts } from "../services/products";
+import { getProducts } from "../services/productService";
 import type { Product } from "../types/Product";
 
 export function useProducts(){
